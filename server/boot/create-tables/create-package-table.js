edc3acc6d8fb95ddf45e1936ce5dbd86c0512c99
MUHAMMAD FURQAN <31932590+FURQAN2050@users.noreply.github.com>
2019-11-09 22:44:08 +0500

    setup of loopback sdk builder.

--- server/boot/create-tables/create-package-table.js
+++ server/boot/create-tables/create-package-table.js
@@ -1,10 +1,14 @@
 'use strict';
 
 module.exports = function (app) {
+    const numModels = Object.keys(app.models).length;
+    for (let dataSource of Object.values(app.dataSources)) {
+        dataSource.setMaxListeners(numModels);
+    }
     var ds = app.dataSources.mySqlDs;
-
+    
     ds.autoupdate('package', function (err) {
         if (err) throw err;
-        
+        console.log(" create package table")
     });
 };
